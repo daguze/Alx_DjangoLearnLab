46b1b1c92427592edc87dccc985d4fb13b033220
Dagim Zemene <Daguzemene@gmail.com>
2025-09-07 17:11:51 +0300

    added function to registration to direct login after registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,8 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.contrib.auth import login
+from django.contrib.auth import logout
 
 # Create your views here.
 
@@ -22,4 +23,10 @@ class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'relationship_app/register.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        # Log the user in after successful registration
+        login(self.request, self.object)
+        return response
 
