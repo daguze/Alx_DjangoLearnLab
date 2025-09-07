ea4adf80b5dd23c17134f1f6fa9dbbbb988b0ab7
Dagim Zemene <Daguzemene@gmail.com>
2025-09-07 16:13:49 +0300

    working books url

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Author, Book
 from .models import Library
 from django.views.generic.detail import DetailView
@@ -15,4 +14,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    
