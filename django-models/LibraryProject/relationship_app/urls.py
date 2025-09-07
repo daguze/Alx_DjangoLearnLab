05dad70e77d5e58721eefae349f403a8df8b0a2b
Dagim Zemene <Daguzemene@gmail.com>
2025-09-07 15:29:03 +0300

    urls import line error

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from . import views
+from .views import list_books
+from .views import LibraryDetailview
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.libraryDetailview.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailview.as_view(), name='library_detail'),
 ]
