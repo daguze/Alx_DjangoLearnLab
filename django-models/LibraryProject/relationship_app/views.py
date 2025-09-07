c41d59784777485cf6df8ad6afc0596023ed1f15
Dagim Zemene <Daguzemene@gmail.com>
2025-09-07 15:19:40 +0300

    fixed view.py function list books

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,7 +9,7 @@ from django.views.generic import DetailView
 def list_books(request):
     books = Book.objects.all()
     book_list = [f"{book.title} by {book.author.name}" for book in books]
-    return HttpResponse(f"{book_list}")
+    return render(request, "relationship_app/list_books.html", {"books": book_list})
 
 class libraryDetailview(DetailView):
     model = Library
