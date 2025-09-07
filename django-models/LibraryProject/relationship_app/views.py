7cfa33ca57a56319a15d3bcd630a5a95c0b9d39d
Dagim Zemene <Daguzemene@gmail.com>
2025-09-07 15:21:26 +0300

    same as before

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Author, Book, Library
+from .models import Author, Book
+from .models import Library
 from django.views.generic import DetailView
 
 
