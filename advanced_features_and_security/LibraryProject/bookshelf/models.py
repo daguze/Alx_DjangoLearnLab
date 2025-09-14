c1913890726ea8918e460c9c26a57bafbe6eacea
Dagim Zemene <Daguzemene@gmail.com>
2025-09-14 14:14:39 +0300

    3rd attempt

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -14,3 +14,29 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+class CustomUserManager(BaseUserManager):
+    def create_user(self,email,username,password=None):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not username:
+            raise ValueError("Users must have an username")
+        
+        user = self.model(
+            email = self.normalize_email(email),
+            username = username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,username,password=None):
+        user = self.create_user(
+            email=email,
+            username=username,
+            password=password
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
