adc49d6f5caf87b5ed21afdc8f675eafaf756ab0
Dagim Zemene <Daguzemene@gmail.com>
2025-09-14 14:16:17 +0300

    4 attempt "the problem was i was woking in relation ship app but it must have been in bookshelf

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -48,36 +48,3 @@ class UserProfile(models.Model):
         return f"{self.user.username} - {self.role}"
     
     # BookForm should be defined outside the UserProfile model
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-
-
-    def __str__(self):
-        return self.username
-
-class CustomUserManager(BaseUserManager):
-    def create_user(self,email,username,password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-        if not username:
-            raise ValueError("Users must have an username")
-        
-        user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self,email,username,password=None):
-        user = self.create_user(
-            email=email,
-            username=username,
-            password=password
-        )
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
