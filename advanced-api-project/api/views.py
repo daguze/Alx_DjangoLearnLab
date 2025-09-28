6cd08a08a34afee50871a24c2eca9559630c52bd
Dagim Zemene <Daguzemene@gmail.com>
2025-09-28 12:55:26 +0300

    import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,8 @@ from .serializers import BookSerializer
 from rest_framework import permissions, generics, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
+from rest_framework import generics
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
