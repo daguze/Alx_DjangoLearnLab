ce43e1161babc046ec58ab3022e9405699b18fc1
Dagim Zemene <Daguzemene@gmail.com>
2025-09-28 12:40:38 +0300

    permission class updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.exceptions import PermissionDenied
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import permissions, generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 # Create your views here.
 
 class BookListView(generics.ListAPIView):
