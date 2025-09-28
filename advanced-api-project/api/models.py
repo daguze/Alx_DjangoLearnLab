d7436025e306f2396bb0f14e2a77c7ef26c03271
Dagim Zemene <Daguzemene@gmail.com>
2025-09-28 12:13:26 +0300

    c

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=120)
+    publication_year = models.IntegerField()
+
+
+    def __str__(self):
+        return self.title
