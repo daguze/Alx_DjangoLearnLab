34b0061a386819b04f184f25a1a13ee5aecf8592
Dagim Zemene <Daguzemene@gmail.com>
2025-10-05 11:08:44 +0300

    database

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '4321',
     }
 }
+
 
 
 # Password validation
